4b7384b7c2243877dc17c50ba75f2635ec31706b
igknighton <trent.knighton@gmail.com>
2025-09-06 11:43:46 -0500

    log and import removal

--- client/src/components/CustomButton.jsx
+++ client/src/components/CustomButton.jsx
@@ -1,10 +1,9 @@
 import * as React from 'react';
 import { styled } from '@mui/material/styles';
 import Button from '@mui/material/Button';
-import { purple } from '@mui/material/colors';
 
 
-const ColorButton = styled(Button)(({ theme }) => ({
+const ColorButton = styled(Button)(({ }) => ({
     color: "#ffffff",
     backgroundColor: "#5e6a5b",
     '&:hover': {
@@ -13,6 +12,5 @@ const ColorButton = styled(Button)(({ theme }) => ({
 }));
 
 export default function CustomButton(props) {
-    console.log("Props",props)
     return <ColorButton {...props} variant={"contained"}>{props.children}</ColorButton>;
 }
